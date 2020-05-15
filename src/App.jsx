e4850cd8f2f4c657e979d4628aab637afeea6b04
thr0wn <natan@bitcoin.com>
2020-05-15 19:27:17 -0300

    fix the cards order in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => (
             <Text fontSize="xsmall" color="grey2">
               R$&nbsp;
               <Text fontSize="medium" color="green">
-                4,65
+                47,32
               </Text>
             </Text>
           </div>
@@ -42,41 +42,6 @@ const App = () => (
             </Text>
             <Text fontSize="medium">
               05/12&nbsp;<Text>Quarta-feira</Text>
-            </Text>
-          </div>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardHeader>
-          <Text fontSize="medium">
-            FATURA&nbsp;
-            <Text fontSize="small" color="grey">
-              JAN 20
-            </Text>
-          </Text>
-          <Tag backgroundColor="purple">Aberta</Tag>
-        </CardHeader>
-        <CardBody>
-          <div>
-            <Text fontFamily="medium" fontSize="large">
-              300&nbsp;
-              <Text fontSize="xsmall" color="grey">
-                MB
-              </Text>
-            </Text>
-            <Text fontSize="xsmall" color="grey2">
-              R$&nbsp;
-              <Text fontSize="medium" color="purple">
-                4,65
-              </Text>
-            </Text>
-          </div>
-          <div>
-            <Text fontSize="small" color="grey">
-              VENCIMENTO
-            </Text>
-            <Text fontSize="medium">
-              05/02&nbsp;<Text>Quarta-feira</Text>
             </Text>
           </div>
         </CardBody>
@@ -120,6 +85,41 @@ const App = () => (
             EFETUAR PAGAMENTO
           </Text>
         </CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <Text fontSize="medium">
+            FATURA&nbsp;
+            <Text fontSize="small" color="grey">
+              JAN 20
+            </Text>
+          </Text>
+          <Tag backgroundColor="purple">Aberta</Tag>
+        </CardHeader>
+        <CardBody>
+          <div>
+            <Text fontFamily="medium" fontSize="large">
+              300&nbsp;
+              <Text fontSize="xsmall" color="grey">
+                MB
+              </Text>
+            </Text>
+            <Text fontSize="xsmall" color="grey2">
+              R$&nbsp;
+              <Text fontSize="medium" color="purple">
+                4,65
+              </Text>
+            </Text>
+          </div>
+          <div>
+            <Text fontSize="small" color="grey">
+              VENCIMENTO
+            </Text>
+            <Text fontSize="medium">
+              05/02&nbsp;<Text>Quarta-feira</Text>
+            </Text>
+          </div>
+        </CardBody>
       </Card>
     </HorizontalScroller>
   </ThemeProvider>
